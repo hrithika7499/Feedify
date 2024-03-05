89feaa6225dd8aaefb86c536782d68de75406b51
Hrithika <rhrithikha04@gmail.com>
2024-03-05 17:05:28 +0530

    card container

---
+++ script.js
@@ -0,0 +1,4 @@
+const API_KEY="33f06768fbf247e89dd9236433c59e3f";
+const url="https://newsapi.org/v2/everything?q=tesla&from=2024-02-05&sortBy=publishedAt&apiKey=33f06768fbf247e89dd9236433c59e3f";
+
+windows.addEventListener("load",()=>fetchNews(India));
